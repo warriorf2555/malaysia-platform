76c53fc35bd5679d88e03dce445bab6a1413103e
warriorf2555 <faiqfisol@gmail.com>
2023-05-29 01:02:19 +0800

    Add manticore into UI

---
+++ src/pages/ApplicationHeader.tsx
@@ -0,0 +1,98 @@
+import React from "react";
+import {
+  createStyles,
+  Header,
+  Container,
+  Group,
+  Button,
+  rem,
+} from "@mantine/core";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+// import { api } from "~/utils/api";
+
+const HEADER_HEIGHT = rem(60);
+
+const useStyles = createStyles((theme) => ({
+  header: {
+    backgroundColor: theme.fn.variant({
+      variant: "filled",
+      color: theme.primaryColor,
+    }).background,
+    borderBottom: 0,
+  },
+
+  inner: {
+    height: HEADER_HEIGHT,
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+
+  links: {
+    [theme.fn.smallerThan("sm")]: {
+      display: "none",
+    },
+  },
+
+  burger: {
+    [theme.fn.largerThan("sm")]: {
+      display: "none",
+    },
+  },
+
+  link: {
+    display: "block",
+    lineHeight: 1,
+    padding: `${rem(8)} ${rem(12)}`,
+    borderRadius: theme.radius.sm,
+    textDecoration: "none",
+    color:
+      theme.colorScheme === "dark"
+        ? theme.colors.dark[0]
+        : theme.colors.gray[7],
+    fontSize: theme.fontSizes.sm,
+    fontWeight: 500,
+
+    "&:hover": {
+      backgroundColor:
+        theme.colorScheme === "dark"
+          ? theme.colors.dark[6]
+          : theme.colors.gray[0],
+    },
+  },
+
+  linkLabel: {
+    marginRight: rem(5),
+  },
+}));
+
+function ApplicationHeader() {
+  const { classes } = useStyles();
+
+  return (
+    <Header
+      height={HEADER_HEIGHT}
+      sx={{ borderBottom: 0 }}
+      className={classes.header}
+    >
+      <Container className={classes.inner} fluid>
+        <Group spacing={5} className={classes.links}>
+          <h5>My App</h5>
+        </Group>
+
+        <Button radius="xl" h={30}>
+          <SignedIn>
+            {/* Mount the UserButton component */}
+            <UserButton afterSignOutUrl="/" />
+          </SignedIn>
+          <SignedOut>
+            {/* Signed out users get sign in button */}
+            <SignInButton />
+          </SignedOut>
+        </Button>
+      </Container>
+    </Header>
+  );
+}
+
+export default ApplicationHeader;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,34 @@
 import { type AppType } from "next/app";
 import { ClerkProvider } from "@clerk/nextjs";
+import Head from "next/head";
+import { MantineProvider } from "@mantine/core";
 import { api } from "~/utils/api";
 import "~/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-    <ClerkProvider {...pageProps}>
-      <Component {...pageProps} />
-    </ClerkProvider>
+    <>
+      <Head>
+        <title>eShop</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+
+      <ClerkProvider {...pageProps}>
+        <MantineProvider
+          withGlobalStyles
+          withNormalizeCSS
+          theme={{
+            /** Put your mantine theme override here */
+            colorScheme: "light",
+          }}
+        >
+          <Component {...pageProps} />
+        </MantineProvider>
+      </ClerkProvider>
+    </>
   );
 };
 
