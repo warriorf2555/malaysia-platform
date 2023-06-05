20a9a3f6a2dc9f4687b3ba5c8ad42ad01d8e4569
warriorf2555 <faiqfisol@gmail.com>
2023-06-05 22:37:25 +0800

    Add index on path

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ import { type NextPage } from "next";
 import ApplicationHeader from "../common/ApplicationHeader";
 import Footer from "../common/Footer";
 // import LandingPage from "~/landingPage";
-import LandingPage from "~/landingPage";
+import LandingPage from "~/landingPage/index";
 
 const Home: NextPage = () => {
   // const hello = api.example.hello.useQuery({ text: "from tRPC" });
